c3c0c1c90b839544d5afc2841ee633d79b1fe7ba
Alexander Kharchenko <morontt@yandex.ru>
2025-02-20 00:18:56 +0300

    add source files

---
+++ src/Exception/TelegramException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: morontt
+ * Date: 24.09.17
+ * Time: 11:09
+ */
+
+namespace Xelbot\Telegram\Exception;
+
+use Exception;
+
+class TelegramException extends Exception
+{
+}
